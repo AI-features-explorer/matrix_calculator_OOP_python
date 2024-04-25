aa6a1e935987db1c882e63445afac79af1ea24db
User_Name <docf37@gmail.com>
2024-04-25 18:59:43 +0300

    add custom matrix exeptions

--- matrix.py
+++ matrix.py
@@ -7,6 +7,22 @@ class MatrixError(Exception):
         super().__init__(message)
         self.message = message
 
+
+class DimensionError(MatrixError):
+    """Exception arising from matrix dimension error"""
+    pass
+
+
+class MultiplicationError(MatrixError):
+    """Exception occurring when trying to multiply incompatible matrices"""
+    pass
+
+
+class DeterminantError(MatrixError):
+    """Exception arising when trying to calculate the determinant of a non-square matrix"""
+    pass
+
+
 class Matrix:
     def __init__(self, matrix):
         self.matrix = np.array(matrix)
